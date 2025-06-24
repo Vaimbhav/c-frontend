ae6750c0cb0061ff6ea098cf147ee4f1dacf5a71
Vaibhav <162452133+Vaimbhav@users.noreply.github.com>
2025-06-24 17:29:47 +0530

    Update ChatPage.jsx

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar'
 const ChatPage = () => {
     return (
         <div>
-            <Navbar />
+{/*             <Navbar /> */}
             <Chat />
         </div>
     )
